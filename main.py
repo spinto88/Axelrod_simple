245a04346e6adba39b0a5517ebf8b0e77346a9c2
spinto <spinto@df.uba.ar>
2016-04-20 13:57:53 -0300

    Rama master actualizada

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 
 from axelrod_py import *
 
-N = 1024
+N = 64
 F = 10
 
 rand.seed(123457)
 
+
+fp = open('Data_smax.txt', 'a')
+fp.write('#q\tsmax\tstd\n')
+fp.close()
+
 for q in range(10, 100, 5):
 
-    mysys = Axl_network(N, F, q, id_topology = 0.1)
+    smax_data = []
 
-    mysys.number_of_metric_feats = 1
+    for conf in range(0, 50):
 
-    mysys.evol2convergence()
+        mysys = Axl_network(N, F, q, id_topology = 0.1)
 
-    smax, max_state = mysys.fragment_identifier()
+        mysys.number_of_metric_feats = 1
 
-    print q, smax
+        mysys.evol2convergence()
+
+        smax, max_state = mysys.fragment_identifier()
+
+        smax_data.append(smax)
+
+    fp = open('Data_smax.txt', 'a')
+    fp.write(str(q) + '\t' + str(np.mean(smax_data)) + '\t' + str(np.std(smax_data)) + '\n')
+    fp.close()
