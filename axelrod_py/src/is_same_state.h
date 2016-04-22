d323e9a8387a5b636f3824c43b3bb6ed40932208
spinto <spinto@df.uba.ar>
2016-04-22 18:16:46 -0300

    Identificador de fragmentos para el caso con radio de clusterizacion, incorporado y funcionando

--- axelrod_py/src/is_same_state.h
+++ axelrod_py/src/is_same_state.h
@@ -2,9 +2,10 @@
 #ifndef IS_SAME_STATE_H
 #define IS_SAME_STATE_H
 
+#include <stdlib.h>
 #include "axelrod.h"
 #include "homophily.h"
 
-int is_same_state(axl_agent, axl_agent);
+int is_same_state(axl_network, int, int, int);
 
 #endif
