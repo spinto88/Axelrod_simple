757a9cbaf294a28cf3d072709a0111b92f4f4a38
spinto <spinto@df.uba.ar>
2016-05-16 19:41:43 -0300

    Main para hacer histogramas interactivo

--- grafico_histogramas.py
+++ grafico_histogramas.py
@@ -3,27 +3,27 @@ from axelrod_py import *
 N = 1024
 F = 10
 Q = 20
-b = 0.25
+b = 0.05
 Z = 20
 
+# Red cuadrada sin CPC
 id_topology = 1.1
 
-rand.seed(123451)
+rand.seed(123457)
 
 A = zealots_list(N, Z)
 
 mysys = Axl_network(n = N, f = F, q = Q, b = b, A = A, id_topology = id_topology, number_of_metric_feats = 1)
 
+# Modo del campo externo: 0.reactivo, 1.activo
+mysys.mode_mf = 1
+
+# Grafica en forma interactiva
 plt.ion()
 
 for i in range(0, 101):
 
     mysys.adherents_hist()
 
-    mysys.evolution(10000)
+    mysys.evolution(100)
 
-    plt.title('Z = 20 Phi = 0.30 Step = ' + str(i * 10000))
-
-#print mysys.evol2stationary()
-
-#mysys.adherents_hist()
